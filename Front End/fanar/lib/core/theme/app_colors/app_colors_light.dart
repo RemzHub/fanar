d3b8cb70bcbc62abd6728d9168d112921161b02b
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-04-22 11:58:32 +0300

    Update app_colors_light.dart

--- Front End/fanar/lib/core/theme/app_colors/app_colors_light.dart
+++ Front End/fanar/lib/core/theme/app_colors/app_colors_light.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColorsLight {
-  static const Color primaryColor = Color(0xFF4CAF50);
+  static const Color primaryColor = Color(0xFFF89C49);
   static const Color secondaryColor = Color(0xFFFEFEFE);
+  static const Color scaffoldBackgroundColor = Color(0xFFFDFDFD);
 
   static const LinearGradient lightGradient = LinearGradient(
     colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
